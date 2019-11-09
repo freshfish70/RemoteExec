8be2dc2d89afb143ba852f43ba9b34e61263425d
Christoffer <christoffer.andersen@live.com>
2019-11-09 15:51:37 +0100

    Add parser to client processor

--- src/loaders/server.ts
+++ src/loaders/server.ts
@@ -3,17 +3,19 @@ import { createClient } from '@/service/network/createJsonSocket'
 import { ClientProcessor } from '@/service/network/ClientProcessor'
 import { Store } from 'vuex'
 import { config } from '@/config'
+import { ProtocolParser } from '@/service/protocol/ProtocolParser'
 /**
  * Initialize the server and
  * @param store Vuex store
  */
 export const createServer = async function createServer(store: Store<{}>) {
-	const processor = new ClientProcessor(store)
+    const parser = new ProtocolParser()
+    const processor = new ClientProcessor(parser)
 
-	let server = new net.Server((socket: Socket) => {
-		createClient(socket, processor)
-	})
+    let server = new net.Server((socket: Socket) => {
+        createClient(socket, processor)
+    })
 
-	// !NEEDS TO BE TRIGGERED BY UI
-	server.listen(config.network.port)
+    // !NEEDS TO BE TRIGGERED BY UI
+    server.listen(config.network.port)
 }
--- src/service/network/ClientProcessor.ts
+++ src/service/network/ClientProcessor.ts
@@ -1,9 +1,6 @@
-import { EventEmitter } from 'events'
-import { JsonSocketProtocol } from './JsonSocketProtocol'
-import { Store } from 'vuex'
-import { DataPackage } from './PackageType'
-import { Socket } from 'net'
 import { Client } from './Client'
+import { ClientPayload } from '../../types/protocol/ClientPayload'
+import { ProtocolParser } from '../protocol/ProtocolParser'
 
 /**
  * The client processor is responsible for managing messages between client
@@ -12,75 +9,70 @@ import { Client } from './Client'
  * @author Christoffer Andersen Træen
  */
 export class ClientProcessor {
-	/**
-	 * Holds all active clients.
-	 * the key is their ID
-	 */
-	private _clients: Map<string, Client> = new Map()
+    /**
+     * Holds all active clients.
+     * the key is their ID
+     */
+    private _clients: Map<string, Client> = new Map()
 
-	/**
-	 * Vuex store
-	 */
-	private _store: Store<{}>
+    /**
+     * ProtocolParser
+     */
+    private _parser: ProtocolParser
 
-	/**
-	 * Initialize the emitter and sets the store
-	 * @param store Vuex store
-	 */
-	constructor(store: Store<{}>) {
-		this._store = store
-	}
+    /**
+     * Initialize the emitter and sets the store
+     * @param store Vuex store
+     */
+    constructor(parser: ProtocolParser) {
+        this._parser = parser
+    }
 
-	/**
-	 * Adds a client to the client pool
-	 */
-	public addClient(client: Client) {
-		this._clients.set(client.id, client)
-	}
+    /**
+     * Adds a client to the client pool
+     */
+    public addClient(client: Client) {
+        this._clients.set(client.id, client)
+    }
 
-	/**
-	 * Removes a client from the pool.
-	 * @param cliendId the id of the client
-	 */
-	private removeClient(cliendId: string) {
-		this._clients.delete(cliendId)
-	}
+    /**
+     * Removes a client from the pool.
+     * @param cliendId the id of the client
+     */
+    private removeClient(cliendId: string) {
+        this._clients.delete(cliendId)
+    }
 
-	/**
-	 * Handles the recieved data
-	 * @param data recieved data
-	 */
-	public recieve(clientid: string, data: DataPackage) {
-		this.parse(data)
-	}
+    /**
+     * Handles the recieved data
+     * @param data recieved data
+     */
+    public recieve(clientid: string, data: ClientPayload) {
+        this._parser.parse(clientid, data)
+    }
 
-	/**
-	 * Handles socket errors
-	 * @param error error object
-	 */
-	public socketError(clientId: string, error: Error) {
-		//!TODO HANDLE SOCKET ERROR
-	}
+    /**
+     * Handles socket errors
+     * @param error error object
+     */
+    public socketError(clientId: string, error: Error) {
+        //!TODO HANDLE SOCKET ERROR
+    }
 
-	/**
-	 * Notifies the UI of a client disconnection.
-	 * Also removes the client from the pool.
-	 * @param clientid the client id
-	 */
-	public clientDisconnected(clientid: string) {
-		//!TODO: NOTIFY REMOVAL OF CLIENT
-		this._clients.delete(clientid)
-		console.log('Client disocnnect: ' + clientid)
-	}
+    /**
+     * Notifies the UI of a client disconnection.
+     * Also removes the client from the pool.
+     * @param clientid the client id
+     */
+    public clientDisconnected(clientid: string) {
+        //!TODO: NOTIFY REMOVAL OF CLIENT
+        this.removeClient(clientid)
+        console.log('Client disocnnect: ' + clientid)
+    }
 
-	/**
-	 * Handles the data to be sent
-	 * @param data data to send
-	 */
-	public send(clientid: string, data: Object) {}
-
-	private parse(data: DataPackage): void {
-		//TODO HANDLE CLIENT DATA
-		console.log(data)
-	}
+    /**
+     * Handles the data to be sent
+     * @param data data to send
+     */
+    public send(clientid: string, data: Object) {}
 }
