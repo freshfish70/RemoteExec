f55d1618d55d34267d710bccc11e9516076cf4b9
Christoffer A Træen <christoffer.andersen@live.com>
2019-12-24 14:31:55 +0100

    Add new app storage inti

--- src/service/storage/appStorage.ts
+++ src/service/storage/appStorage.ts
@@ -1,11 +1,26 @@
-import EStore from 'electron-store'
+import Store from 'electron-store'
 /**
  * Store for app sensitive data, like
  * paths, passwords, IP whitelist etc.
+ *
+ * Setups the store if it is not initialized. If it is
+ * try to get the initialized value from the sore; it will return undefined
+ * if the password is wrong, hence wrong password.
+ * And will throw and Error if it cant open the storage.
  */
-const appStorage = (password: string) => {
+const appStorage = (password: string, initialize: boolean) => {
 	let passwordBuffer = Buffer.from(password)
-	return new EStore({ name: 'app', encryptionKey: passwordBuffer })
+	let storage = new Store({ name: 'app', encryptionKey: passwordBuffer })
+	if (initialize) {
+		storage.set('initialized', true)
+	}
+
+	let opened = storage.get('initialized')
+	if (!opened) throw new Error('Could not open secure storage.')
+	return {
+		remove,
+		set,
+	}
 }
 
 export { appStorage }
