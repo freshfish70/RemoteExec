96676231591b0d429c4e3ac39abe0a3991defff2
Christoffer A Træen <christoffer.andersen@live.com>
2019-12-27 18:26:52 +0100

    Extended routeconfig to require title and subtitle meta

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,28 +1,29 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue'
+import VueRouter, { RouterOptions, RouteConfig } from 'vue-router'
+import Home from '../views/Home.vue'
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: "/",
-    name: "home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
-];
+interface RouteConfigExtended extends RouteConfig {
+	meta: {
+		title: string
+		subtitle: string
+	}
+}
+const routes: RouteConfigExtended[] = [
+	{
+		path: '/',
+		name: 'home',
+		component: Home,
+		meta: {
+			title: 'Home',
+			subtitle: 'home subtitle',
+		},
+	},
+]
 
 const router = new VueRouter({
-  routes
-});
+	routes,
+})
 
-export default router;
+export default router
