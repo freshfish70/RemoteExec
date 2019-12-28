7031ed6a79459c09ec997187a11f6e47fe8a6248
Christoffer A Træen <christoffer.andersen@live.com>
2019-12-28 13:17:08 +0100

    Add routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import VueRouter, { RouterOptions, RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import Main from '../views/Main.vue'
+import Clients from '../views/Clients.vue'
+import Sequences from '../views/Sequences.vue'
+import Settings from '../views/Settings.vue'
 
 Vue.use(VueRouter)
 
@@ -8,17 +12,57 @@ interface RouteConfigExtended extends RouteConfig {
 	meta: {
 		title: string
 		subtitle: string
+		protected?: boolean
 	}
 }
 const routes: RouteConfigExtended[] = [
 	{
 		path: '/',
-		name: 'home',
-		component: Home,
+		name: 'Login',
+		component: Login,
 		meta: {
-			title: 'Home',
-			subtitle: 'home subtitle',
+			title: 'Login',
+			subtitle: '',
+			protected: false,
 		},
+	},
+	{
+		path: '/app',
+		component: Main,
+		meta: {
+			title: '',
+			subtitle: '',
+			protected: true,
+		},
+		children: [
+			{
+				path: '/',
+				name: 'Clients',
+				component: Clients,
+				meta: {
+					title: 'Clients',
+					subtitle: 'Clients',
+				},
+			},
+			{
+				path: 'sequences',
+				name: 'Sequences',
+				component: Sequences,
+				meta: {
+					title: 'Sequences',
+					subtitle: 'sequences',
+				},
+			},
+			{
+				path: 'settings',
+				name: 'Settings',
+				component: Settings,
+				meta: {
+					title: 'Settings',
+					subtitle: 'settings',
+				},
+			},
+		],
 	},
 ]
 
