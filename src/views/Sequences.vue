a31010ec338665d61ff2cc7b2bcf926857f1eefa
Christoffer A Træen <christoffer.andersen@live.com>
2020-02-10 21:39:31 +0100

    Sequences

--- src/views/Sequences.vue
+++ src/views/Sequences.vue
@@ -1,6 +1,109 @@
 <template>
-	<div>SEQUECNES</div>
+	<div id="page-content">
+		<section id="content-top">
+			<ul class="client-info">
+				<li>129.123.123.232</li>
+				<li>2001:db8:85a3:0:0:8a2e:370:7334</li>
+			</ul>
+		</section>
+		<section id="content" class="">
+			<b-table
+				borderless
+				class="table-compact"
+				:items="items"
+				:fields="fields"
+			>
+				<template v-slot:cell(running)="data">
+					<div
+						class="connection-state connected"
+						v-if="data.value"
+					></div>
+					<div class="connection-state disconnected" v-else></div>
+				</template>
+				<template v-slot:cell(toggle)="data">
+					<img
+						class="toggle-image"
+						src="/images/icons/play.png"
+						v-if="data.value"
+					/>
+					<img
+						class="toggle-image"
+						src="/images/icons/stop.png"
+						v-else
+					/>
+				</template>
+				<template v-slot:cell(view)="data">
+					<router-link to="/app/client/4/execution/IDHERE"
+						><img class="toggle-image" src="/images/icons/eye.png"
+					/></router-link>
+				</template>
+			</b-table>
+		</section>
+	</div>
 </template>
 <script>
-export default {}
+import { Component, Vue } from 'vue-property-decorator'
+import Card from '@/components/card/card'
+import ExecutionFormModal from '@/components/ExecutionFormModal.vue'
+@Component({
+	components: {
+		clientCard: Card,
+		executionFormModal: ExecutionFormModal,
+	},
+})
+export default class Sequences extends Vue {
+	fields = [
+		{ key: 'select', label: '', thClass: 'table-icon-column' },
+		{
+			key: 'executable',
+		},
+		{
+			key: 'arguments',
+		},
+		{
+			key: 'delay',
+		},
+		{
+			key: 'status',
+		},
+		{
+			key: 'run',
+			thClass: 'table-icon-column',
+		},
+		{
+			key: 'edit',
+			thClass: 'table-icon-column',
+		},
+	]
+	items = [
+		{
+			executable: 'test.exe',
+			arguments: '-arg 1 fullscreen',
+			delay: '5',
+			status: 'running',
+			run: true,
+		},
+		{
+			executable: 'text.exe',
+			arguments: '-arg 1 fullscreen',
+			delay: '6',
+			status: 'Waiting',
+			run: false,
+		},
+		{
+			executable: 'test.exe',
+			arguments: '-arg 1 fullscreen',
+			delay: '2        ',
+			status: 'Stopped',
+			run: true,
+		},
+	]
+}
 </script>
+<style lang="scss" scoped>
+input[type='text'],
+input[type='text']:focus {
+	height: 3.2rem;
+	margin: -0.7rem 0 0 0;
+}
+</style>
