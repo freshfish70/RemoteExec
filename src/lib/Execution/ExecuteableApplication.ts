02533d475a74c01fc74b2afa2d8db8fabcea02ee
Christoffer A Traeen <christoffer.andersen@live.com>
2020-04-27 14:32:38 +0200

    Add name,description and state to executable app

--- src/lib/Execution/ExecuteableApplication.ts
+++ src/lib/Execution/ExecuteableApplication.ts
@@ -1,26 +1,45 @@
+import { ProcessState } from './ProcessState'
+
 /**
  * Represents an executeable application on a remote client
  */
 export class ExecuteableApplication {
 	// Unique id used to identify the execution
 	private _eid: string = ''
+
+	// The name of the process /executable
+	private _name: string = ''
+
+	// Description of the process, what it does?
+	private _description: string = ''
+
 	// The application to execute
 	private _application: string = ''
+
 	// The path on the remote system for the application
 	private _path: string = ''
+
 	// Arguments for the application
 	private _arguments: string = ''
+
 	// Delays in milliseconds
 	private _delay: number = 0
 
+	// The state of the process
+	private _processState: ProcessState = ProcessState.STOPPED
+
 	constructor(
 		eid: string,
+		name: string,
+		description: string,
 		application: string,
 		path: string,
 		args: string,
 		delay: number
 	) {
 		this._eid = eid
+		this._name = name
+		this._description = description
 		this.application = application
 		this.path = path
 		this.arguments = args
@@ -29,6 +48,22 @@ export class ExecuteableApplication {
 
 	public get eid(): string {
 		return this._eid
+	}
+
+	public get name(): string {
+		return this._name
+	}
+
+	public set name(name: string) {
+		this._name = name
+	}
+
+	public get description(): string {
+		return this._description
+	}
+
+	public set description(description: string) {
+		this._description = this._description
 	}
 
 	public get application(): string {
@@ -61,5 +96,13 @@ export class ExecuteableApplication {
 
 	public set delay(v: number) {
 		this._delay = v
+	}
+
+	public get processState(): ProcessState {
+		return this._processState
+	}
+
+	public set processState(state: ProcessState) {
+		this._processState = state
 	}
 }
