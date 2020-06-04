9487ab40a2fc22845b090e6a00714fdc11a0f9be
Christoffer A Traeen <christoffer.andersen@live.com>
2020-06-04 21:49:00 +0200

    Refactor setup handler for connected clients

--- src/service/protocol/handlers/SetupHandler.ts
+++ src/service/protocol/handlers/SetupHandler.ts
@@ -16,37 +16,34 @@ type SetupCompletePayload = {
 }
 
 /**
- * Creates a new client and adds the client to the store
+ * Creates a new client
  * @param name name of the client
  */
 function createClient(name: string = 'Unnamed'): FrontClient {
 	const id = uuidv4()
 	const client = new FrontClient(name, id)
-	client.connected = true
-	ClientStore.addClient(client)
 	return client
 }
 
 export default (client: Client, payload: SetupPayload) => {
-	let frontClient: FrontClient
-
-	if (!payload.id) {
-		frontClient = createClient(payload.name)
-	} else {
-		const client = ClientStore.client(payload.id)
-		if (!client) {
-			frontClient = createClient(payload.name)
-		}
-		frontClient = client as FrontClient
+	const currentTime = new Date().toISOString()
+	let storeClient = ClientStore.client(payload.id)
+	let newClient = false
+	if (!storeClient) {
+		storeClient = createClient(payload.name)
+		storeClient.firstSeen = currentTime
+		newClient = true
 	}
 
-	const currentTime = new Date().toISOString()
-	frontClient.connected = true
-	frontClient.firstSeen = currentTime
-	frontClient.lastSeen = currentTime
-	frontClient.validExecutionFolders = payload.executableTree
+	storeClient.connected = true
+	storeClient.lastSeen = currentTime
+	storeClient.validExecutionFolders = payload.executableTree
 
 	client.socket.on('close', () => {
-		frontClient.connected = false
+		storeClient!.connected = false
 	})
+
+	if (newClient) {
+		ClientStore.addClient(storeClient)
+	}
 }
