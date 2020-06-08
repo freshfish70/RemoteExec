5e309e35365be394364b12a7423e7dc7153d611f
Christoffer A Traeen <christoffer.andersen@live.com>
2020-06-08 23:12:16 +0200

    Store and load clients

--- src/loaders/storageLoader.ts
+++ src/loaders/storageLoader.ts
@@ -1,18 +1,48 @@
 import Clients from '@/store/modules/Clients'
 import { Client } from '@/lib/client/Client'
 import { generalStorage } from '@/service/storage/generalStore'
+import { ExecuteableApplication } from '@/lib/Execution/ExecuteableApplication'
+import { Executable } from '@/lib/Execution/Executable'
+import { StoredClient } from '@/lib/client/StoredClient'
+
+import { ClientGroupExecution } from '@/lib/Execution/ClientGroupExecution'
 
 /**
  * Loads all saved clients from storage into vuex Clients store
  */
 function loadClients() {
-	const loadedClients = generalStorage.getClients()
+	const loadedClients: Array<StoredClient> = generalStorage.getClients()
 	const createdClients: Array<Client> = []
 	if (loadedClients) {
 		for (const client of loadedClients) {
-			const createdClient = Object.assign(new Client(), client)
-			createdClient.connected = false
-			createdClients.push(createdClient)
+			let cl = new Client(client.name, client.id)
+
+			for (const groupExecution of client.groupExecutions) {
+				let newGroupExecution = new ClientGroupExecution(
+					groupExecution.name,
+					groupExecution.description
+				)
+				let executables = new Set<Executable>()
+				for (const storedExecutable of groupExecution.executables) {
+					let executableApp = new ExecuteableApplication(
+						storedExecutable.executableApplication.eid,
+						storedExecutable.executableApplication.name,
+						storedExecutable.executableApplication.description,
+						storedExecutable.executableApplication.application,
+						storedExecutable.executableApplication.path,
+						storedExecutable.executableApplication.arguments
+					)
+					let executable = new Executable(
+						executableApp,
+						storedExecutable.delay
+					)
+					cl.executions.push(executableApp)
+					executables.add(executable)
+				}
+				newGroupExecution.executables = executables
+				cl.addGroupExecution(newGroupExecution)
+			}
+			createdClients.push(cl)
 		}
 		Clients.setAndReplaceClients(createdClients)
 	}
--- src/service/storage/generalStore.ts
+++ src/service/storage/generalStore.ts
@@ -1,5 +1,8 @@
 import Store from 'electron-store'
 import { Client } from '@/lib/client/Client'
+import { StoredClient } from '@/lib/client/StoredClient'
+import { MultiExecutable } from '@/types/Processes/MultiExecutable'
+import { Application } from '@/types/Processes/Application'
 
 /**
  * Store for basic app data which is not sensitive.
@@ -28,11 +31,70 @@ class GeneralStorage extends Storage {
 	}
 
 	getClients() {
-		return this.store.get('clients') as Array<Client>
+		return this.store.get('clients') as Array<StoredClient>
 	}
 
 	saveClients(clients: Array<Client>) {
-		return this.store.set('clients', clients)
+		let transformedClients: Array<StoredClient> = []
+
+		for (const client of clients) {
+			let storedClient: StoredClient = {
+				name: '',
+				id: '',
+				ipAddresses: {
+					ipv4: '',
+					ipv6: '',
+				},
+				firstSeen: '',
+				lastSeen: '',
+				validExecutionFolders: [],
+				groupExecutions: [],
+			}
+
+			storedClient.name = client.name
+			storedClient.id = client.id
+			storedClient.ipAddresses = client.ipAddresses
+			storedClient.firstSeen = client.firstSeen
+			storedClient.lastSeen = client.lastSeen
+
+			let groupExecutions = []
+			for (const groupExecution of client.groupExecutions) {
+				let transformedGroupExecution: MultiExecutable = {
+					name: '',
+					description: '',
+					executables: [],
+				}
+
+				transformedGroupExecution.name = groupExecution.name
+				transformedGroupExecution.description =
+					groupExecution.description
+
+				let applications: Array<Application> = []
+				for (const ex of groupExecution.executables.values()) {
+					const app = {
+						eid: ex.executableApplication.eid,
+						name: ex.executableApplication.name,
+						description: ex.executableApplication.description,
+						application: ex.executableApplication.application,
+						path: ex.executableApplication.path,
+						arguments: ex.executableApplication.arguments,
+					}
+					applications.push(app)
+					let executable = {
+						id: ex.id,
+						delay: ex.delay,
+						executableApplication: app,
+					}
+					transformedGroupExecution.executables.push(executable)
+				}
+
+				groupExecutions.push(transformedGroupExecution)
+			}
+			storedClient.groupExecutions = groupExecutions
+
+			transformedClients.push(storedClient)
+		}
+		return this.store.set('clients', transformedClients)
 	}
 }
 const generalStorage = new GeneralStorage(storage)
