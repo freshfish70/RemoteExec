633120960f76e2e5ac8c65b911204ed3fd59ca83
Christoffer A Traeen <christoffer.andersen@live.com>
2020-06-08 17:15:28 +0200

    Implement store binder to setup handler for socket

--- src/service/protocol/handlers/SetupHandler.ts
+++ src/service/protocol/handlers/SetupHandler.ts
@@ -1,9 +1,10 @@
 import { Client } from '@/service/network/Client'
 import { v4 as uuidv4 } from 'uuid'
 
-import ClientStore from '@/store/modules/Clients'
-import { Client as FrontClient } from '@/lib/client/Client'
 import { ExecutableTree } from '@/types/Clients/ExecutableTree'
+
+import ClientStoreBinder from '@/service/binder/ClientStoreBinder'
+import clientStoreBinder from '@/service/binder/ClientStoreBinder'
 
 type SetupPayload = {
 	id: string
@@ -15,37 +16,19 @@ type SetupCompletePayload = {
 	id: string
 }
 
-/**
- * Creates a new client
- * @param name name of the client
- */
-function createClient(name: string = 'Unnamed'): FrontClient {
-	const id = uuidv4()
-	const client = new FrontClient(name, id)
-	return client
-}
-
 export default (client: Client, payload: SetupPayload) => {
-	const currentTime = new Date().toISOString()
-	let storeClient = ClientStore.client(payload.id)
-	let newClient = false
-	if (!storeClient) {
-		storeClient = createClient(payload.name)
-		storeClient.firstSeen = currentTime
-		newClient = true
+	let id = payload.id
+
+	if (clientStoreBinder.clientExists(id)) {
+		ClientStoreBinder.clientConnected(id)
+	} else {
+		id = uuidv4()
+		clientStoreBinder.createNewClient(payload.name, id)
 	}
 
-	storeClient.connected = true
-	storeClient.lastSeen = currentTime
-	storeClient.validExecutionFolders = payload.executableTree
-
-	client.id = storeClient.id
+	client.id = id
 
 	client.socket.on('close', () => {
-		storeClient!.connected = false
+		clientStoreBinder.clientDisconnected(id)
 	})
-
-	if (newClient) {
-		ClientStore.addClient(storeClient)
-	}
 }
